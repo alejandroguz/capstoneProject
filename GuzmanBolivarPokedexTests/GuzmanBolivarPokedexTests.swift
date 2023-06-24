1e7ff6fa432a392164bc9b52fc13853c98a438dd
Alejandro Guzman <ketokunxd@gmail.com>
2023-06-23 22:20:31 -0500

    Add: Tests

--- GuzmanBolivarPokedexTests/GuzmanBolivarPokedexTests.swift
+++ GuzmanBolivarPokedexTests/GuzmanBolivarPokedexTests.swift
@@ -6,31 +6,65 @@
 //
 
 import XCTest
+import SwiftUI
 @testable import GuzmanBolivarPokedex
 
 final class GuzmanBolivarPokedexTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  override func setUpWithError() throws {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+  }
+
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
+
+  func test_Networking_getRequestURL_ShouldGetAnURLForFetching() {
+    // Given
+    let network = Networking()
+    let expectedURL = "https://pokeapi.co/api/v2/pokemon/25"
+
+    // When
+    let pokemonURL = network.getRequestURL(forPokemonNumber: 25)
+
+    // Then
+    XCTAssertEqual(expectedURL, pokemonURL)
+  }
+
+  func test_Networking_getPokemonProfile_ShouldGetPokedexFromAPICall() async throws {
+    // Given
+    let network = Networking()
+
+    // When
+    let result = try? await network.getPokemonProfile()
+
+    // Then
+    XCTAssertNotNil(result)
+  }
+
+  func test_Networking_getPokemonImg_ShouldGetPokemonImg() async throws {
+    // Given
+    let network = Networking()
+    var img: [UIImage]?
+
+    // When
+    if let pokedexArray = try? await network.getPokemonProfile() {
+      img = try? await network.getPokemonImg(pokemon: pokedexArray)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    // Then
+    XCTAssertNotNil(img)
+  }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
+  func test_PokemonEncounterNetworking_getPokemonEncounterInfo_GetPokemonDetailInfo() async throws {
+    // Given
+    let pokemonEncounterNetworking = PokemonEncounterNetworking()
+    let url = "https://pokeapi.co/api/v2/pokemon/6/encounters"
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    // When
+    let result = try? await pokemonEncounterNetworking.getPokemonEncounterInfo(url: url)
+    // Then
+    XCTAssertNotNil(result)
+    XCTAssertNoThrow(result)
+  }
 }
--- GuzmanBolivarPokedexUITests/GuzmanBolivarPokedexUITests.swift
+++ GuzmanBolivarPokedexUITests/GuzmanBolivarPokedexUITests.swift
@@ -9,33 +9,38 @@ import XCTest
 
 final class GuzmanBolivarPokedexUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+  let app = XCUIApplication()
 
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+  override func setUpWithError() throws {
+    continueAfterFailure = false
+    app.launch()
+  }
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
+  override func tearDownWithError() throws {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+  }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+  func test_ContentView_TabBar_TabBarExistAndWorks() {
+    // Given
+    let tabBar = app.tabBars["Tab Bar"]
+    let introTabButton = tabBar.buttons["Intro"]
+    let listTabButton = tabBar.buttons["List"]
+    let profileTabButton = tabBar.buttons["Profile"]
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
+    // Then
+    XCTAssertTrue(introTabButton.isSelected)
+    XCTAssertFalse(listTabButton.isSelected)
+    XCTAssertFalse(profileTabButton.isSelected)
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    listTabButton.tap()
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
+    XCTAssertTrue(listTabButton.isSelected)
+    XCTAssertFalse(introTabButton.isSelected)
+    XCTAssertFalse(profileTabButton.isSelected)
+
+    profileTabButton.tap()
+    XCTAssertFalse(listTabButton.isSelected)
+    XCTAssertFalse(introTabButton.isSelected)
+    XCTAssertTrue(profileTabButton.isSelected)
+  }
 }
